e42536fd42986339b67f92841d987c375a8ec71e
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-30 18:46:48 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,30 +1,9 @@
 import random
-from sympy import isprime
 
-def gen_prime():
-    while True:
-        p = random.randint(100, 300)
-        if isprime(p):
-            return p
-
-p = gen_prime()
-q = gen_prime()
-
-n = p * q
-phi = (p - 1) * (q - 1)
-
+p = random.randint(100,300)
+q = random.randint(100,300)
+n = p*q
 e = 3
-while phi % e == 0:
-    e += 2
 
-def modinv(a, m):
-    for x in range(1, m):
-        if (a * x) % m == 1:
-            return x
-
-d = modinv(e, phi)
-
-open("../keys/public.key", "w").write(f"{e},{n}")
-open("../keys/private.key", "w").write(f"{d},{n}")
-
-print("[CLIENT] Clés RSA générées")
+open("../keys/public.key","w").write(f"{e},{n}")
+print("[CLIENT] Clés générées")
