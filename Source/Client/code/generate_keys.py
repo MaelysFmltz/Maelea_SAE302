4b6a5c116b81a5695b11fcabd5241943f1fdf9e5
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 00:18:19 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,41 +1,8 @@
-import random
-import os
-
-CLIENT_NAME = input("Nom du client : ")
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
-
-def gen_prime():
-    while True:
-        p = random.randint(100, 300)
-        if is_prime(p):
-            return p
-
-p = gen_prime()
-q = gen_prime()
-n = p * q
-phi = (p-1)*(q-1)
-
-e = 3
-while phi % e == 0:
-    e += 2
-
-def inv(a, m):
-    for x in range(1, m):
-        if (a*x) % m == 1:
-            return x
-
-d = inv(e, phi)
+import os, random
 
 os.makedirs("../keys", exist_ok=True)
 
-open(f"../keys/{CLIENT_NAME}.public", "w").write(f"{e},{n}")
-open(f"../keys/{CLIENT_NAME}.private", "w").write(f"{d},{n}")
+key = bytes(random.randint(0,255) for _ in range(32))
+open("../keys/session.key","wb").write(key)
 
-print(f"[CLIENT {CLIENT_NAME}] Clés générées")
+print("[CLIENT] Clé de session générée")
