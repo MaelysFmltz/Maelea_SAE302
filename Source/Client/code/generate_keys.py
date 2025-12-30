6f5c1da76e5e8681c608c345961d3966eea52a0e
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-30 20:00:05 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,28 +1,6 @@
 import random
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+key = bytes(random.randint(0,255) for _ in range(16))
+open("../keys/session.key","wb").write(key)
 
-def gen_prime():
-    while True:
-        p = random.randint(100, 300)
-        if is_prime(p):
-            return p
-
-p = gen_prime()
-q = gen_prime()
-n = p * q
-
-e = 3
-while (p - 1) * (q - 1) % e == 0:
-    e += 2
-
-with open("../keys/public.key", "w") as f:
-    f.write(f"{e},{n}")
-
-print("[CLIENT] Clé publique générée")
+print("[CLIENT] Clé de session générée")
