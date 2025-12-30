0b743ef6ba7a48c9bcbd235ac937fdfc69e8c1c7
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-30 19:06:06 +0100

    SAE

--- Source/Router/code/router.py
+++ Source/Router/code/router.py
@@ -11,21 +11,30 @@ def rsa_decrypt(data):
     return "".join(chr(pow(int(x), d, n)) for x in data.split(";") if x)
 
 def handle(conn):
-    enc = conn.recv(65536).decode()
-    text = rsa_decrypt(enc)
-    header, payload = text.split("\n",1)
+    try:
+        enc = conn.recv(65536).decode()
+        text = rsa_decrypt(enc)
+    except:
+        conn.close()
+        return
+
+    if "\n" not in text:
+        conn.close()
+        return
+
+    header, payload = text.split("\n", 1)
 
     if header.startswith("NEXT"):
         _, ip, port = header.split()
         s = socket.socket()
-        s.connect((ip,int(port)))
+        s.connect((ip, int(port)))
         s.sendall(payload.encode())
         s.close()
 
     elif header.startswith("FINAL"):
         _, ip, port = header.split()
         s = socket.socket()
-        s.connect((ip,int(port)))
+        s.connect((ip, int(port)))
         s.sendall(payload.encode())
         s.close()
 
@@ -34,8 +43,8 @@ def handle(conn):
 s = socket.socket()
 s.bind(("0.0.0.0", LISTEN_PORT))
 s.listen(5)
-print(f"[{ROUTER_NAME}] OK")
+print(f"[{ROUTER_NAME}] En écoute")
 
 while True:
-    c,_ = s.accept()
-    threading.Thread(target=handle,args=(c,),daemon=True).start()
+    c, _ = s.accept()
+    threading.Thread(target=handle, args=(c,), daemon=True).start()
