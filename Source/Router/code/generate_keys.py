07f26fac2120ed2371e731d7f30654d63b0c24d1
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-30 21:47:17 +0100

    SAE

--- Source/Router/code/generate_keys.py
+++ Source/Router/code/generate_keys.py
@@ -1,14 +1,25 @@
 import random
-from sympy import isprime
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 def gen_prime():
     while True:
         p = random.randint(100, 300)
-        if isprime(p):
+        if is_prime(p):
             return p
 
-p = gen_prime()
-q = gen_prime()
+while True:
+    p = gen_prime()
+    q = gen_prime()
+    if p != q:
+        break
+
 n = p * q
 phi = (p - 1) * (q - 1)
 
@@ -20,10 +31,15 @@ def inv(a, m):
     for x in range(1, m):
         if (a * x) % m == 1:
             return x
+    return None
 
 d = inv(e, phi)
+
+if d is None:
+    print("Erreur génération clé, relance le script")
+    exit(1)
 
 open("../keys/public.key", "w").write(f"{e},{n}")
 open("../keys/private.key", "w").write(f"{d},{n}")
 
-print("[ROUTER] Clés RSA générées")
+print("[ROUTER] Clés RSA générées correctement")
