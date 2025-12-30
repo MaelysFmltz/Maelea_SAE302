533ea0b49768eaa3e6c9fc589319186c4c6b534d
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-30 19:08:55 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,9 +1,28 @@
 import random
 
-p = random.randint(100,300)
-q = random.randint(100,300)
-n = p*q
-e = 3
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-open("../keys/public.key","w").write(f"{e},{n}")
-print("[CLIENT] Clés générées")
+def gen_prime():
+    while True:
+        p = random.randint(100, 300)
+        if is_prime(p):
+            return p
+
+p = gen_prime()
+q = gen_prime()
+n = p * q
+
+e = 3
+while (p - 1) * (q - 1) % e == 0:
+    e += 2
+
+with open("../keys/public.key", "w") as f:
+    f.write(f"{e},{n}")
+
+print("[CLIENT] Clé publique générée")
