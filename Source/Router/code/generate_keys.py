3f3e8ea52395061451b31f647531c5ec381db58b
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 01:00:21 +0100

    SAE

--- Source/Router/code/generate_keys.py
+++ Source/Router/code/generate_keys.py
@@ -1,39 +1,35 @@
-import random
-import os
+import random, os
 
-ROUTER_NAME = input("Nom du routeur (ex: R1): ")
-
+ROUTER_NAME = input("Nom du routeur (ex: R1) : ")
 os.makedirs("../keys", exist_ok=True)
 
 def is_prime(n):
-    if n < 2:
-        return False
+    if n < 2: return False
     for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
+        if n % i == 0: return False
     return True
 
 def gen_prime():
     while True:
-        p = random.randint(100, 300)
+        p = random.randint(100,300)
         if is_prime(p):
             return p
 
 p = gen_prime()
 q = gen_prime()
-n = p * q
+n = p*q
 phi = (p-1)*(q-1)
 
 e = 3
 while phi % e == 0:
     e += 2
 
-def inv(a, m):
-    for x in range(1, m):
+def inv(a,m):
+    for x in range(1,m):
         if (a*x) % m == 1:
             return x
 
-d = inv(e, phi)
+d = inv(e,phi)
 
 open(f"../keys/{ROUTER_NAME}.public","w").write(f"{e},{n}")
 open(f"../keys/{ROUTER_NAME}.private","w").write(f"{d},{n}")
