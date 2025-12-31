3f447f09cc5340c06d5813281521c0262be7c907
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 16:53:18 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,12 +1,16 @@
-import random, os
+import random
+import os
 
-CLIENT_NAME = input("Nom du client (ex: C1): ")
+CLIENT_NAME = input("Nom du client (ex: C1) : ")
+
 os.makedirs("../keys", exist_ok=True)
 
 def is_prime(n):
-    if n < 2: return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0: return False
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
     return True
 
 def gen_prime():
@@ -17,21 +21,14 @@ def gen_prime():
 
 p = gen_prime()
 q = gen_prime()
-n = p*q
-phi = (p-1)*(q-1)
+n = p * q
+phi = (p - 1) * (q - 1)
 
 e = 3
 while phi % e == 0:
     e += 2
 
-def inv(a,m):
-    for x in range(1,m):
-        if (a*x) % m == 1:
-            return x
+with open(f"../keys/{CLIENT_NAME}.public", "w") as f:
+    f.write(f"{e},{n}")
 
-d = inv(e, phi)
-
-open(f"../keys/{CLIENT_NAME}.public","w").write(f"{e},{n}")
-open(f"../keys/{CLIENT_NAME}.private","w").write(f"{d},{n}")
-
-print(f"[{CLIENT_NAME}] Clés RSA générées")
+print(f"[{CLIENT_NAME}] Clé publique générée")
