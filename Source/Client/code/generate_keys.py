d49538cf881ddf80b7a97604142d54020e51c7f5
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 15:59:42 +0100

    SAE

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,6 +1,10 @@
 import os, random
 
+CLIENT_NAME = input("Nom du client : ")
+
 os.makedirs("../keys", exist_ok=True)
+
 key = bytes(random.randint(0,255) for _ in range(32))
-open("../keys/session.key","wb").write(key)
-print("[CLIENT] Clé de session générée")
+open(f"../keys/{CLIENT_NAME}.session","wb").write(key)
+
+print(f"[{CLIENT_NAME}] Clé de session générée")
