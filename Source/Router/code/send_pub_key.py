7db82f74422844d43c76cc673c39e2a42ed6f81b
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 01:00:59 +0100

    SAE

--- Source/Router/code/send_pub_key.py
+++ Source/Router/code/send_pub_key.py
@@ -8,14 +8,14 @@ MASTER_IP = input("IP du master : ")
 MASTER_PORT = int(input("Port du master : "))
 
 with open(f"../keys/{ROUTER_NAME}.public") as f:
-    pubkey = f.read().strip()
+    pub = f.read().strip()
 
 msg = (
     f"ROUTEUR {ROUTER_NAME}\n"
     f"IP {ROUTER_IP}\n"
     f"PORT {ROUTER_PORT}\n"
     "PUBKEY\n"
-    f"{pubkey}\n"
+    f"{pub}\n"
     "END\n"
 )
 
@@ -24,4 +24,4 @@ s.connect((MASTER_IP, MASTER_PORT))
 s.sendall(msg.encode())
 s.close()
 
-print(f"[{ROUTER_NAME}] Clé publique envoyée")
+print(f"[{ROUTER_NAME}] Clé envoyée au master")
