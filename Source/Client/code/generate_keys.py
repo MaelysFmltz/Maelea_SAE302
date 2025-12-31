a5d4fa84ff7e31d45702188c0615812d886a0919
R107_FOMHOLTZMaelys <maelys.fomholtz@gmail.com>
2025-12-31 16:16:39 +0100

    Update generate_keys.py

--- Source/Client/code/generate_keys.py
+++ Source/Client/code/generate_keys.py
@@ -1,10 +1,37 @@
-import os, random
+import random, os
 
-CLIENT_NAME = input("Nom du client : ")
-
+CLIENT_NAME = input("Nom du client (ex: C1): ")
 os.makedirs("../keys", exist_ok=True)
 
-key = bytes(random.randint(0,255) for _ in range(32))
-open(f"../keys/{CLIENT_NAME}.session","wb").write(key)
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
 
-print(f"[{CLIENT_NAME}] Clé de session générée")
+def gen_prime():
+    while True:
+        p = random.randint(100, 300)
+        if is_prime(p):
+            return p
+
+p = gen_prime()
+q = gen_prime()
+n = p*q
+phi = (p-1)*(q-1)
+
+e = 3
+while phi % e == 0:
+    e += 2
+
+def inv(a,m):
+    for x in range(1,m):
+        if (a*x) % m == 1:
+            return x
+
+d = inv(e, phi)
+
+open(f"../keys/{CLIENT_NAME}.public","w").write(f"{e},{n}")
+open(f"../keys/{CLIENT_NAME}.private","w").write(f"{d},{n}")
+
+print(f"[{CLIENT_NAME}] Clés RSA générées")
